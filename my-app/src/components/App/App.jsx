4a2a0e27dc24418815c05c5fab4a066b959f19f7
Vladimir Vasilev <bobastart777@mail.ru>
2024-01-21 21:20:30 +0300

    JSON placeholder

--- my-app/src/components/App/App.jsx
+++ my-app/src/components/App/App.jsx
@@ -1,110 +1,37 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useState } from "react"
 import styles from "./App.module.css"
-import { useForm } from 'react-hook-form'
-import * as yup from 'yup'
-import { yupResolver } from '@hookform/resolvers/yup'
-
-const sendFormData = (formData) => {
-    console.log(formData);
-}
-
-const fieldsSchema = yup.object()
-    .shape({
-        email: yup.string()
-            .matches(/^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/, 'Некорректный email'),
-        password: yup.string()
-            .matches(/^[a-zA-Z0-9]+$/, 'Пароль может содержать только латинские буквы и цифры')
-            .min(3, 'Пароль должен быть не меньше 3 символов')
-            .max(10, 'Пароль должен быть не больше 10 символов'),
-        confirmPassword: yup.string()
-            .oneOf([yup.ref("password")], 'Пароли не совпадают')
-            .required('Необходимо подтвердить пароль'),
-})
 
 export const App = () => {
-	const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        reset
-    } = useForm({
-        defaultValues: {
-            email: '',
-            password: '',
-            confirmPassword: '',
-        },
-        resolver: yupResolver(fieldsSchema),
-    })
+	const [products, setProducts] = useState([])
+	const [isLoading, setIsLoading] = useState(false)
 
-	const btn = useRef(null)
+    useEffect(() => {
+        setIsLoading(true);
 
-    const resetForm = () => {
-        reset()
-	}
-
-    const emailError = errors.email?.message
-    const passwordError = errors.password?.message
-    const confirmPasswordError = errors.confirmPassword?.message
-
-	useEffect(() => {
-		if(!confirmPasswordError) {
-			btn.current.focus()
-		}
-	})
+		setTimeout(() => {
+			fetch('https://jsonplaceholder.typicode.com/todos')
+				.then((loadedData) => loadedData.json())
+				.then((loadedProducts) => {
+					setProducts(loadedProducts);
+				})
+				.finally(() => setIsLoading(false))
+		}, 3000)
+    }, [])
 
     return (
         <div className={styles.app}>
-            <form onSubmit={handleSubmit(sendFormData)} className={styles.form}>
-                <label
-                    htmlFor="email"
-                >
-                    Email
-                </label>
-				<input
-					type="email"
-					name="email"
-					placeholder="test@test.com"
-					{...register('email')}
-				/>
-                {emailError && <div className={styles.errorLabel}>{emailError}</div>}
-                <label
-                    htmlFor="password"
-                >
-                    Пароль
-                </label>
-                <input
-					type="password"
-					name="password"
-                    placeholder="***"
-					{...register('password')}
-				/>
-                {passwordError && <div className={styles.errorLabel}>{passwordError}</div>}
-                <label
-                    htmlFor="confirmPassword"
-                >
-                    Повтор пароля
-                </label>
-                <input
-					type="password"
-					name="confirmPassword"
-                    placeholder="***"
-					{...register('confirmPassword')}
-				/>
-                {confirmPasswordError && <div className={styles.errorLabel}>{confirmPasswordError}</div>}
-				<div className={styles.btnGroup}>
-					<button
-						type="submit"
-						disabled={!!passwordError}
-						className={styles.formBtn}
-						ref={btn}
-					>Зарегистрироваться</button>
-					<button
-					type="button"
-						className={styles.formBtnRed}
-						onClick={resetForm}
-					>Сброс</button>
-				</div>
-            </form>
-        </div>
+			<h1>Список дел</h1>
+			<ul className={styles.todo}>
+			{isLoading ? (
+				<div className={styles.loader}></div>
+			) : (
+				products.map(({ id, title }) => (
+					<li key={id} className={styles.todoItem}>
+						{title}
+					</li>
+				))
+			)}
+			</ul>
+		</div>
     )
 }
